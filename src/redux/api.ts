615d9a405902eed2cb8b9c1375a5c634bb8cd374
Praphool <81462169+Prap1@users.noreply.github.com>
2026-04-19 15:15:42 +0530

    Update api.ts

--- src/redux/api.ts
+++ src/redux/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://192.168.31.75:8000/api/',
+  baseURL: 'http://192.168.31.75:8000/api/',
   headers: {
     'Content-Type': 'application/json',
   },
@@ -64,7 +64,7 @@ api.interceptors.response.use(
       }
 
       try {
-        const response = await axios.post('https://192.168.31.75:8000/api/auth/token/refresh/', {
+        const response = await axios.post('http://192.168.31.75:8000/api/auth/token/refresh/', {
           refresh: refreshToken,
         });
 
